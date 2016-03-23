1712572aab3f3c7ac575d86ae942438730f06f88
Armando <apiconz@gmail.com>
2016-03-23 01:51:22 -0500

    Create a RecyclerView and Adapter with Kotlin

---
+++ app/src/main/java/com/devpicon/android/kotlinweatherapp/ForecastListAdapter.kt
@@ -0,0 +1,24 @@
+package com.devpicon.android.kotlinweatherapp
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * Created by Armando on 3/23/2016.
+ */
+class ForecastListAdapter(val items: List<String>) :
+        RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(TextView(parent.context))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+}
--- app/src/main/java/com/devpicon/android/kotlinweatherapp/MainActivity.kt
+++ app/src/main/java/com/devpicon/android/kotlinweatherapp/MainActivity.kt
@@ -2,19 +2,32 @@ package com.devpicon.android.kotlinweatherapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val items = listOf<String>("Mon 6/23 - Sunny - 31/17",
+            "Tue 6/24 - Foggy - 21/8",
+            "Wed 6/25 - Cloudy - 22/17",
+            "Thurs 6/26 - Rainy - 18/11",
+            "Fri 6/27 - Foggy - 21/10",
+            "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+            "Sun 6/29 - Sunny - 20/7"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        message.text = "Hello Kotlin!"
-        toast(message.text.toString())
+
+        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+        forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.adapter = ForecastListAdapter(items)
+
     }
 
-    fun toast(message: String, length: Int = Toast.LENGTH_SHORT){
+    fun toast(message: String, length: Int = Toast.LENGTH_SHORT) {
         Toast.makeText(this, message, length).show()
     }
 }

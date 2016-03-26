3fa3e061e6d22dc7af9e3ce2bc580038f24814a6
Armando <picondev@gmail.com>
2016-03-26 17:18:00 -0500

    Parsing Data

--- app/src/main/java/com/devpicon/android/kotlinweatherapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/devpicon/android/kotlinweatherapp/ui/activities/MainActivity.kt
@@ -15,15 +15,6 @@ import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
 
-    private val items = listOf<String>("Mon 6/23 - Sunny - 31/17",
-            "Tue 6/24 - Foggy - 21/8",
-            "Wed 6/25 - Cloudy - 22/17",
-            "Thurs 6/26 - Rainy - 18/11",
-            "Fri 6/27 - Foggy - 21/10",
-            "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-            "Sun 6/29 - Sunny - 20/7"
-    )
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -31,18 +22,13 @@ class MainActivity : AppCompatActivity() {
         val forecastList: RecyclerView = find(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
 
-
-        val url = "http://api.openweathermap.org/data/2.5/forecast/daily?" +
-                "APPID=b7a56bb43570189115cb8b2d98cdde5b&q=94043&mode=json&units=metric&cnt=7"
-
         async() {
             val result = RequestForecastCommand("94043").execute()
             uiThread {
                 forecastList.adapter = ForecastListAdapter(result)
-                longToast("Request performed") }
+                longToast("Request performed")
+            }
         }
-
-
     }
 
     fun toast(message: String, length: Int = Toast.LENGTH_SHORT) {

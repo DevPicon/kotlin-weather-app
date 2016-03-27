c6331839e8304307dc92cc27dc7b9eb0416aa4f3
Armando <picondev@gmail.com>
2016-03-27 00:36:14 -0500

    Operator overloading

--- app/src/main/java/com/devpicon/android/kotlinweatherapp/domain/model/DomainClasses.kt
+++ app/src/main/java/com/devpicon/android/kotlinweatherapp/domain/model/DomainClasses.kt
@@ -3,5 +3,9 @@ package com.devpicon.android.kotlinweatherapp.domain.model
 /**
  * Created by Reapro on 26/03/2016.
  */
-data class ForecastList(val city: String, val country: String, val dailyForecast:List<Forecast>)
+data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>) {
+    operator fun get(position: Int): Forecast = dailyForecast[position]
+    fun size(): Int = dailyForecast.size
+}
+
 data class Forecast(val date: String, val description: String, val high: Int, val low: Int)

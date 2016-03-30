a2ae1998b1c0d7dd5a4d261371ae7e6e909ea611
Armando <apiconz@gmail.com>
2016-03-29 22:18:17 -0500

    using Kotlin Android Extensions

--- app/src/main/java/com/devpicon/android/kotlinweatherapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/devpicon/android/kotlinweatherapp/ui/activities/MainActivity.kt
@@ -3,12 +3,11 @@ package com.devpicon.android.kotlinweatherapp.ui.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import com.devpicon.android.kotlinweatherapp.R
 import com.devpicon.android.kotlinweatherapp.domain.commands.RequestForecastCommand
 import com.devpicon.android.kotlinweatherapp.ui.adapters.ForecastListAdapter
+import kotlinx.android.synthetic.main.activity_main.forecastList
 import org.jetbrains.anko.async
-import org.jetbrains.anko.find
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 
@@ -18,7 +17,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val forecastList: RecyclerView = find(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
 
         async() {

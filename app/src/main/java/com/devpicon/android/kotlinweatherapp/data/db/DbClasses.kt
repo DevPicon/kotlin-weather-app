640ec707dfcecc011f7ec300a3a0c9f6701e5911
Armando <apiconz@gmail.com>
2016-04-18 17:24:00 -0500

    Saving and requesting data from database

--- app/src/main/java/com/devpicon/android/kotlinweatherapp/data/db/DbClasses.kt
+++ app/src/main/java/com/devpicon/android/kotlinweatherapp/data/db/DbClasses.kt
@@ -10,7 +10,8 @@ class CityForecast(val map: MutableMap<String, Any?>, val dailyForecast: List<Da
     var city: String by map
     var country: String by map
 
-    constructor(id: Long, city: String, country: String, dailyForecast: List<DayForecast>) : this(HashMap(), dailyForecast) {
+    constructor(id: Long, city: String, country: String, dailyForecast: List<DayForecast>)
+    : this(HashMap(), dailyForecast) {
         this._id = id
         this.city = city
         this.country = country
